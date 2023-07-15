8bf35ddd1d6518826103845dd4a8ee7145f82538
CheechRockwizard <github@lamcat.co.uk>
2023-07-15 14:07:03 +0100

    image column extension

--- inc/blocks/lc_three_images.php
+++ inc/blocks/lc_three_images.php
@@ -1,20 +1,23 @@
-<section class="three_images__container">
+<?php
+$class = get_field('columns') == 2 ? 'two' : 'three';
+?>
+<section class="<?=$class?>_images__container">
     <?php
     if (get_field('container')) {
         echo '<div class="container-xl">';
     }
-    ?>
-    <div class="three_images">
+?>
+    <div class="<?=$class?>_images">
         <?php
-    while (have_rows('images')) {
-        the_row();
-        echo wp_get_attachment_image(get_sub_field('image'), 'large');
-    }
-    ?>
+while (have_rows('images')) {
+    the_row();
+    echo wp_get_attachment_image(get_sub_field('image'), 'large');
+}
+?>
     </div>
     <?php
-    if (get_field('container')) {
-        echo '</div>';
-    }
-    ?>
+if (get_field('container')) {
+    echo '</div>';
+}
+?>
 </section>
