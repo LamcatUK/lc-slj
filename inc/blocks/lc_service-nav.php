6e115830dd18466dc6cb4bb88481625a5d7d9a8c
CheechRockwizard <github@lamcat.co.uk>
2023-09-09 12:41:27 +0100

    service nav rollover

--- inc/blocks/lc_service-nav.php
+++ inc/blocks/lc_service-nav.php
@@ -3,9 +3,15 @@
         <div class="row justify-content-center g-3">
             <div class="col-sm-4">
                 <a href="/project-management-restoration/" class="nav_card">
-                    <img class="nav_card__image"
-                        src="<?=get_hero_image('project-management-restoration')?>"
-                        alt="Project Management & Restoration">
+                    <div class="nav_card__rollover">
+                        <img class="nav_card__image"
+                            src="<?=get_hero_image('project-management-restoration')?>"
+                            alt="Project Management & Restoration">
+                        <div class="nav_card__hover">
+                            <img src="<?=get_stylesheet_directory_uri()?>/img/icon--project-management-restoration.svg"
+                                alt="">
+                        </div>
+                    </div>
                     <div class="nav_card__title">
                         Project Management &amp; Restoration
                     </div>
@@ -13,9 +19,15 @@
             </div>
             <div class="col-sm-4">
                 <a href="/servicing-maintenance/" class="nav_card">
-                    <img class="nav_card__image"
-                        src="<?=get_hero_image('servicing-maintenance')?>"
-                        alt="Servicing & Maintenance">
+                    <div class="nav_card__rollover">
+                        <img class="nav_card__image"
+                            src="<?=get_hero_image('servicing-maintenance')?>"
+                            alt="Servicing & Maintenance">
+                        <div class="nav_card__hover">
+                            <img src="<?=get_stylesheet_directory_uri()?>/img/icon--servicing-maintenance.svg"
+                                alt="">
+                        </div>
+                    </div>
                     <div class="nav_card__title">
                         Servicing &amp; Maintenance
                     </div>
@@ -23,9 +35,15 @@
             </div>
             <div class="col-sm-4">
                 <a href="/engine-building-machining/" class="nav_card">
-                    <img class="nav_card__image"
-                        src="<?=get_hero_image('engine-building-machining')?>"
-                        alt="Engine Building & Machining">
+                    <div class="nav_card__rollover">
+                        <img class="nav_card__image"
+                            src="<?=get_hero_image('engine-building-machining')?>"
+                            alt="Engine Building & Machining">
+                        <div class="nav_card__hover">
+                            <img src="<?=get_stylesheet_directory_uri()?>/img/icon--engine-building-machining.svg"
+                                alt="">
+                        </div>
+                    </div>
                     <div class="nav_card__title">
                         Engine Building &amp; Machining
                     </div>
@@ -33,9 +51,15 @@
             </div>
             <div class="col-sm-4">
                 <a href="/design-development/" class="nav_card">
-                    <img class="nav_card__image"
-                        src="<?=get_hero_image('design-development')?>"
-                        alt="Design & Development">
+                    <div class="nav_card__rollover">
+                        <img class="nav_card__image"
+                            src="<?=get_hero_image('design-development')?>"
+                            alt="Design & Development">
+                        <div class="nav_card__hover">
+                            <img src="<?=get_stylesheet_directory_uri()?>/img/icon--design-development.svg"
+                                alt="">
+                        </div>
+                    </div>
                     <div class="nav_card__title">
                         Design &amp; Development
                     </div>
@@ -43,9 +67,15 @@
             </div>
             <div class="col-sm-4">
                 <a href="/welding-fabrication/" class="nav_card">
-                    <img class="nav_card__image"
-                        src="<?=get_hero_image('welding-fabrication')?>"
-                        alt="Welding & Fabrication">
+                    <div class="nav_card__rollover">
+                        <img class="nav_card__image"
+                            src="<?=get_hero_image('welding-fabrication')?>"
+                            alt="Welding & Fabrication">
+                        <div class="nav_card__hover">
+                            <img src="<?=get_stylesheet_directory_uri()?>/img/icon--welding-fabrication.svg"
+                                alt="">
+                        </div>
+                    </div>
                     <div class="nav_card__title">
                         Welding &amp; Fabrication
                     </div>
@@ -53,9 +83,15 @@
             </div>
             <div class="col-sm-4">
                 <a href="/bristol-car-parts/" class="nav_card">
-                    <img class="nav_card__image"
-                        src="<?=get_hero_image('bristol-car-parts')?>"
-                        alt="Bristol Car Parts">
+                    <div class="nav_card__rollover">
+                        <img class="nav_card__image"
+                            src="<?=get_hero_image('bristol-car-parts')?>"
+                            alt="Bristol Car Parts">
+                        <div class="nav_card__hover">
+                            <img src="<?=get_stylesheet_directory_uri()?>/img/icon--bristol-car-parts.svg"
+                                alt="">
+                        </div>
+                    </div>
                     <div class="nav_card__title">
                         Bristol Car Parts
                     </div>
