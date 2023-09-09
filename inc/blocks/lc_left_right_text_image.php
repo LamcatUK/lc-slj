1ae4b36018da33becf46a0718765369708487aac
CheechRockwizard <github@lamcat.co.uk>
2023-09-09 13:25:37 +0100

    text/image block

--- inc/blocks/lc_left_right_text_image.php
+++ inc/blocks/lc_left_right_text_image.php
@@ -1,31 +1,63 @@
 <?php
+$colour = get_field('theme');
+$background = '';
+switch ($colour) {
+    case 'Dark':
+        $background = 'bg--grey';
+        break;
+    case 'Light':
+        $background = '';
+        break;
+}
 
-$half = get_field('order') == 'text_left' ? 'right-half' : 'left-half';
-$offset = get_field('order') == 'text_left' ? '' : 'offset-lg-6';
+$breakout = $background;
 
+$padding = '';
+if (get_field('padding')[0] ?? null && get_field('padding')[0] == 'Yes') {
+    $padding = 'py-5';
+}
+
+$splitText = 'col-lg-6';
+$splitImage = 'col-lg-6';
+
+if (get_field('split') == '6040') {
+    $splitText = 'col-lg-8';
+    $splitImage = 'col-lg-4';
+}
+if (get_field('split') == '7030') {
+    $splitText = 'col-lg-10';
+    $splitImage = 'col-lg-2';
+}
+
+$orderText = 'order-2 order-lg-1';
+$orderImage = 'order-1 order-lg-2';
+
+if (get_field('order') == 'image-text') {
+    $orderText = 'order-2 order-lg-2';
+    $orderImage = 'order-1 order-lg-1';
+}
+
+$classes = $block['className'] ?? null;
 ?>
-<section class="left_right_text_image container-fluid half-fluid">
-    <div class="container-xl">
-        <div class="row">
+<section
+    class="text_image <?=$breakout?> <?=$padding?> <?=$classes?>">
+    <div class="container-xl <?=$background?>">
+        <div class="d-lg-none">
+            <h2><?=get_field('title')?></h2>
+        </div>
+        <div class="row align-items-center g-4 gx-lg-5">
             <div
-                class="col-lg-6 position-lg-absolute <?=$half?> h-100">
-                <?=wp_get_attachment_image(get_field('image'), 'full', false, [ 'class' => 'fit' ])?>
+                class="<?=$splitText?> <?=$orderText?>">
+                <h2 class="d-none d-lg-block">
+                    <?=get_field('title')?>
+                </h2>
+                <div class="mb-4">
+                    <?=get_field('content')?>
+                </div>
             </div>
             <div
-                class="col-lg-6 <?=$offset?> py-5 ps-lg-5 text-half">
-                <div class="h-100 d-flex flex-column justify-content-center">
-                    <?php
-                    if (get_field('title')) {
-                        ?>
-                    <h2><?=get_field('title')?>
-                    </h2>
-                    <?php
-                    }
-?>
-                    <div>
-                        <?=get_field('content')?>
-                    </div>
-                </div>
+                class="<?=$splitImage?> <?=$orderImage?> text-center">
+                <?=wp_get_attachment_image(get_field('image'), 'large', null, array('class' => 'text_image__image'))?>
             </div>
         </div>
     </div>
--- inc/blocks/lc_nav_cards.php
+++ inc/blocks/lc_nav_cards.php
@@ -1,26 +1,26 @@
-<section class="nav_cards py-5">
+<!-- <section class="nav_cards py-5">
     <div class="container-xl">
         <div class="row justify-content-center g-1">
             <?php
             while (have_rows('pages')) {
                 the_row();
                 ?>
-            <div class="col-sm-4 col-lg-3">
-                <a href="<?=get_the_permalink(get_sub_field('page_link'))?>"
-                    class="nav_card">
-                    <div class="nav_card__image">
-                        <img src="<?=wp_get_attachment_image_url(get_sub_field('card_image'), 'large')?>"
-                            alt="<?=get_the_title(get_sub_field('page_link'))?>"
-                            class="w-100">
-                    </div>
-                    <div class="nav_card__title">
-                        <?=get_the_title(get_sub_field('page_link'))?>
-                    </div>
-                </a>
-            </div>
-            <?php
+<div class="col-sm-4 col-lg-3">
+    <a href="<?=get_the_permalink(get_sub_field('page_link'))?>"
+        class="nav_card">
+        <div class="nav_card__image">
+            <img src="<?=wp_get_attachment_image_url(get_sub_field('card_image'), 'large')?>"
+                alt="<?=get_the_title(get_sub_field('page_link'))?>"
+                class="w-100">
+        </div>
+        <div class="nav_card__title">
+            <?=get_the_title(get_sub_field('page_link'))?>
+        </div>
+    </a>
+</div>
+<?php
             }
             ?>
-        </div>
-    </div>
-</section>
+</div>
+</div>
+</section> -->
