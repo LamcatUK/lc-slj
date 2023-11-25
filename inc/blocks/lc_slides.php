ced63fa717eebea23475f59dad82456e6fa82930
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2023-11-25 11:59:43 +0000

    slides

--- inc/blocks/lc_slides.php
+++ inc/blocks/lc_slides.php
@@ -1,8 +1,36 @@
 <section class="slides">
     <div class="container-xl d-flex flex-wrap justify-content-center">
-        <img src="<?=get_stylesheet_directory_uri()?>/img/slides-left.png"
-            alt="">
-        <img src="<?=get_stylesheet_directory_uri()?>/img/slides-right.png"
-            alt="">
+        <a href="<?=get_stylesheet_directory_uri()?>/img/slides-left-1.jpg"
+            data-fancybox="slides">
+            <img src="<?=get_stylesheet_directory_uri()?>/img/slides-left-1.png"
+                alt=""></a>
+        <a href="<?=get_stylesheet_directory_uri()?>/img/slides-left-2.jpg"
+            data-fancybox="slides">
+            <img src="<?=get_stylesheet_directory_uri()?>/img/slides-left-2.png"
+                alt=""></a>
+        <a href="<?=get_stylesheet_directory_uri()?>/img/slides-right-1.jpg"
+            data-fancybox="slides">
+            <img src="<?=get_stylesheet_directory_uri()?>/img/slides-right-1.png"
+                alt=""></a>
+        <a href="<?=get_stylesheet_directory_uri()?>/img/slides-right-2.jpg"
+            data-fancybox="slides">
+            <img src="<?=get_stylesheet_directory_uri()?>/img/slides-right-2.png"
+                alt=""></a>
     </div>
 </section>
+<?php
+add_action('wp_footer', function () {
+    ?>
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" />
+<script>
+    Fancybox.bind("[data-fancybox]", {
+        Images: {
+            zoom: false,
+        },
+        hideScrollbar: false
+    });
+</script>
+<?php
+});
+        ?>
